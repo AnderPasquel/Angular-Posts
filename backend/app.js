36799b8c6479ae3ab8231299774e1a99f0cb7d71
AnderPasquel <anderpasquelrosales@gmail.com>
2018-09-10 20:19:26 -0500

    New Login
    Se añadio un login al  proyecto

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const Post = require('./models/post');
+
 const postsRoutes = require('./routes/post');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
@@ -22,14 +24,21 @@ app.use("/images", express.static(path.join("backend/images")));
 
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Header', 'Origin, X-Requested-with, Content-Type, Accept');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+    );
     next();
 });
 
 //const pass = jYUZK9UsCr88dUJ6; 
 
 app.use('/api/posts',postsRoutes);
+app.use('/api/user', userRoutes);
 
 module.exports = app;
